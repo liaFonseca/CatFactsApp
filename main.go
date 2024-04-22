231164473c63fbccca790d65c2448b33ab9753ac
pentefinp <lia_fonseca1997@hotmail.com>
2024-04-22 16:39:38 +0100

    Server set up

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/bndr/gopencils"
+	"github.com/gin-gonic/gin"
 )
 
 type catFacts struct {
@@ -11,6 +12,17 @@ type catFacts struct {
 }
 
 func main() {
+	// creates a gin default server
+	server := gin.Default()
+
+	// creates a Get endpoint (/test)
+	server.GET("/test", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{"message": "OK!"})
+	})
+
+	// starts the server
+	server.Run(":8080")
+
 	// connect to meowfacts api
 	api := gopencils.Api("https://meowfacts.herokuapp.com")
 
@@ -18,7 +30,7 @@ func main() {
 	res := new(catFacts)
 
 	// make a get request and unmarshal json response into response struct
-	_, err := api.Res("", res).Get()
+	_, err := api.Res("", res).Get(map[string]string{"count": "3", "lang": "por-br"})
 
 	if err != nil {
 		fmt.Println("Error:", err)
